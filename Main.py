69199f744300cd360d6e1789495a03007d557bcb
Wisp <146360282+Wisparity@users.noreply.github.com>
2024-03-18 09:26:06 -0600

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,14 @@
+#func def------------------------------
+
+def calculate_distance(speed,time):
+    distance = speed * time
+    print(distance,"kmh")
+    return distance
+
+def calculate_area(length,width):
+    Area = length*width
+    print(Area)
+    return Area
+
+calculate_distance(60,3)
+calculate_area(10,5)
